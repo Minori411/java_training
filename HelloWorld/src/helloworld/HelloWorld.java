e72423382b1bb1ae681f1489670a9f3ab855adaa
whmd0 <whmd0@MSI>
2024-11-16 17:44:49 +0900

    Hello World!

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+/**
+* Hello Worldクラス
+*/
+public class HelloWorld {
+	/**
+	 * mainメソッド
+	 *@param args 実行引数
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
