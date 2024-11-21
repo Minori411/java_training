b30f97aadf00437767ad91c648e5c32caa50b25c
whmd0 <whmd0@MSI>
2024-11-21 22:48:13 +0900

    operator commit

---
+++ operator/src/operator/operator.java
@@ -0,0 +1,21 @@
+package operator;
+
+
+
+class sample4
+{
+	public static void main(String[] args)
+	{
+		int num1 = 0-4;
+		double num2 = 3.14 * 2;
+		double num3 = (double)5 / 3;
+		int num4 = 30 % 7;
+		double num5 = (7 +32) /(double)5;
+		
+		System.out.println("0-4は" + num1 + "です。");
+		System.out.println("3.14 * 2は" + num2 + "です。");
+		System.out.println("5　÷　3は" + num3 + "です。");
+		System.out.println("30 ÷ 7のあまりの数は" + num4 + "です。");
+		System.out.println("(7 + 32) ÷ 5 は" + num5 + "です。");
+	}
+}
